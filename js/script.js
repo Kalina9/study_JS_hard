91595d5ca5fc9d60bf90b69da08daae5fe706197
Evheniy Kalinichenko <kalinichenko0997z@gmail.com>
2020-08-11 07:23:50 +0300

    Использую тернарные операторы для условия

--- js/script.js
+++ js/script.js
@@ -1,18 +1,59 @@
-const num = 266219;
-let total = 1;
+"use strict";
+// const num = 266219;
 
-const array = num
-  .toString()
-  .split("")
-  .map(function (item, index) {
-    return parseInt(item);
-  });
-console.log(array);
+// let total = 1;
 
-for (let i = 0; i < array.length; ++i) {
-  total *= array[i];
-}
+// // const array = num
+// //   .toString()
+// //   .split("")
+// //   .map(function (item, index) {
+// //     return parseInt(item);
+// //   });
+// // console.log(array);
 
-//console.log(total ** 3);
+// // for (let i = 0; i < array.length; ++i) {
+// //   total *= array[i];
+// // }
 
-alert(String(total ** 3).substr(0, 2));
+// //console.log(total ** 3);
+
+// //alert(String(total ** 3).substr(0, 2));
+
+let lang = "en";
+let array = [];
+//выводяться дни недели с ипользованием If
+// if (lang == 'ru') {
+//     array = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+// }
+// if (lang == 'en') {
+//     array = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+// }
+// console.log(array);
+//выводяться дни недели с ипользованием switch
+// switch (lang) {
+//     case 'ru':
+//         array = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+//         break;
+//     case 'en':
+//         array = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+//         break;
+// }
+// console.log(array);
+
+//выводяться дни недели с ипользованием массива
+// let array = {
+//     'ru': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+//     'en': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+// };
+// console.log(array[lang]);
+
+// Использую тернарные операторы, без использования if или switch
+let namePerson = "Женя";
+let result;
+result =
+  namePerson == "Артем"
+    ? "Босс"
+    : namePerson == "Максим"
+    ? "Преподаватель"
+    : "Студент";
+console.log(result);
