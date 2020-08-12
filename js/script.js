ea8e67be040319e22925b43ab4e358b811a3a7fb
Evheniy Kalinichenko <kalinichenko0997z@gmail.com>
2020-08-12 07:36:13 +0300

    Вывел в консоль строку

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let getString = function (string) {
+    if (typeof string !== 'string') {
+        alert('Вы ввели неверные данные');
+        return;
+    }
+    // Удаляю пробелы с концов строки trim()
+    string = string.trim();
+    //Если длина строки > 30 символов добавляю ..., если меньше показываю строку
+    return string.length > 30 ? string.substr(0, 30) + '...' : string;
+    //return string.length > 30 ? string.slice(0, 30) + '...' : string;
+}
+console.log(getString('   dsssssssssssssssssssiissssssssssssssdsdds   '));
